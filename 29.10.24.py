987f0f5a5682b819579d9aad2f888acd0092f19a
Lukasschwegler <lukas.schwegler24@gmail.com>
2024-11-10 10:28:17 +0100

    Update 29.10.24.py

--- 29.10.24.py
+++ 29.10.24.py
@@ -1,31 +1,29 @@
-kreditbetrag = int(input("Kreditbetrag "))
+
+kreditbetrag = float(input("Kreditbetrag "))
 
 jahre = int(input("Laufzeit "))
 
 zinssatz = float(input("Zinssatz in % "))
 
-monatliche_rate = int(input("Monatlicherate "))
-
-#sonder_tilgung = int(input("Sondertilgung "))
+monatliche_rate = float(input("Monatlicherate "))
 
 #Anuität
 # Berechnen der Annuität
 anu = monatliche_rate  * 12
 #print(f"f string: Die Annuität beträgt : {anu:.2f} Euro")
 
-# Zinsen gesamt 
-#zinsen = 
-
-
 kapital = kreditbetrag
+total_interest = 0  # Initialize total interest variable
+repayment = 0  # Initialize total interest variable
 
 for jahr in range(1, jahre + 1):
-    zinsen = kapital/100 * zinssatz  # Berechnung der Zinsen für das Jahr
-    tilgen = anu - zinsen 
-    kapital -= tilgen                # Die Tilgung
-    #kapital -= sonder_tilgung        # Die Sondertilgung
+    zinsen = kapital / 100 * zinssatz  # Calculate interest for the year
+    tilgen = anu - zinsen              # Calculate principal reduction
+    kapital -= tilgen                  # Reduce principal
+    total_interest += zinsen           # Add yearly interest to total interest
+    repayment  += tilgen
+
     print(f"Jahr {jahr}: Zinsen = {zinsen:.2f} EUR, Tilgung = {tilgen:.2f} EUR, Restschuld = {kapital:.2f} EUR")
-   
 
 def kreditrate_berechnen(kreditbetrag, zinssatz, jahre):
     # Zinssatz von Prozent in Dezimal umwandeln und monatlichen Zinssatz berechnen
@@ -41,6 +39,9 @@ def kreditrate_berechnen(kreditbetrag, zinssatz, jahre):
 monatliche = kreditrate_berechnen(kreditbetrag, zinssatz, jahre)
 #print(f"Rate bei Volltilgung: {monatliche:.2f} Euro")
 
+#Gesamtkosten
+gesamtkosten = total_interest + kreditbetrag
+
 # Beispiel-Dictionary
 # Färben des Dictionary
 farbe = "\033[90m"  # Grau
@@ -52,11 +53,11 @@ mein_dict = {
     "Monatsrate": f"{farbe}Meine Monatsrate beträgt: {monatliche_rate:.2f}€{reset}",
     "Rate bei Volltilgung": f"{farbe}Die Volltilgung beträgt: {monatliche:.2f}€{reset}",
     "Zinssatz": f"{farbe}Meine Zinssatz beträgt: {zinssatz:.2f} %{reset}",
-   # "Die gesamten Zinsen": f"{farbe}Die gesamten Zinsen betragen: {zinsen:.2f}€{reset}",
-    "Annuität": f"{farbe}Die Annuität beträgt: {anu:.2f}€{reset}",
-   # "Sondertilgung im Jahr": f"{farbe}Die Sondertilgung beträgt: {sonder_tilgung:.2f}€{reset}",
-   # "Sondertilgung gesamt": f"{farbe}Die gesamte Sondertilgung beträgt: {gesamt:.2f}€{reset}",4
-   # "Gesamtkosten":
+    "Die gesamten Zinsen": f"{farbe}Die gesamten Zinsen betragen: {total_interest:.2f}€{reset}",
+    "Anuität": f"{farbe}Die Anuität beträgt: {anu:.2f}€{reset}",
+   "Die gesamte Tilgung": f"{farbe}Die Tilgung beträgt: {repayment:.2f}€{reset}",
+   "Gesamtkosten": f"{farbe}Die gesamt Gesamtkosten betragen: {gesamtkosten:.2f}€{reset}",
+   
 }
 
 # Manuelle Formatierung
